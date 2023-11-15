9d741c6627ba601a5d4b076cb07639f52dcb6b84
Suvorov <chachukha.aliaksei@gmail.com>
2023-11-15 22:22:07 +0300

    article-page worker-page

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -1,9 +1,9 @@
-// Подключение свайпера
-import { auto } from '@popperjs/core';
-import Swiper, { Autoplay, Navigation, Pagination, Controller, EffectFade, } from 'swiper';
-Swiper.use([Autoplay, Navigation, Pagination, Controller, EffectFade]);
-const mobileSlider = new Swiper('.swiper-one', {
-  slidesPerView: auto,
-  loop: true,
-});
+// // Подключение свайпера
+// import { auto } from '@popperjs/core';
+// import Swiper, { Autoplay, Navigation, Pagination, Controller, EffectFade, } from 'swiper';
+// Swiper.use([Autoplay, Navigation, Pagination, Controller, EffectFade]);
+// const mobileSlider = new Swiper('.swiper-one', {
+//   slidesPerView: auto,
+//   loop: true,
+// });
 
